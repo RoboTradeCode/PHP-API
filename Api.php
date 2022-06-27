f441ec968e03fee83f843c5e28ad53ac0d65b448
IDV <idv@idv.su>
2022-06-27 19:28:38 +0300

    Обновлена функция генерации уникального ID

--- Api.php
+++ Api.php
@@ -58,13 +58,23 @@ class Api
     }
 
     /**
-     * Возвращает уникальный ID на основе timestamp
+     * Возвращает уникальный UUID4
      *
      * @return string
      */
-    public function getUniqID(): string
+    public function generateUUID(): string
     {
-        return uniqid(rand(100, 999) . '-',true);
+        // Generate 16 bytes (128 bits) of random data or use the data passed into the function.
+        $data = random_bytes(16);
+        assert(strlen($data) == 16);
+
+        // Set version to 0100
+        $data[6] = chr(ord($data[6]) & 0x0f | 0x40);
+        // Set bits 6-7 to 10
+        $data[8] = chr(ord($data[8]) & 0x3f | 0x80);
+
+        // Output the 36 character UUID.
+        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
     }
 
     /**
